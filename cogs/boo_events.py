3fcae9eb42b2fb7da92f0e2433f315a5dc2f17d4
ss-samaim <sam.suvichagorn@gmail.com>
2022-07-03 15:12:57 +1000

    Performs reactions to messages defined in the config

--- cogs/boo_events.py
+++ cogs/boo_events.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 import json
+import re
+import random
 
 from utils import repo
 
@@ -29,6 +31,31 @@ class Events(commands.Cog):
         with open('data/prefixes.json', 'w') as f:
             json.dump(prefixes, f, indent=2)
 
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        # Performs reactions as defined in the config
+        for trigger, reaction in repo.REACTIONS.items():
+            if re.search(fr"\b({trigger})\b", message.content, re.IGNORECASE):
+                for emoji in reaction:
+                    await message.add_reaction(emoji)
+
+        # Performs reactions to messages containing "league"
+        if re.search(fr"\b(league)\b", message.content, re.IGNORECASE) and message.author.id != self.bot.user.id:
+            league_response = [
+                "League of Legends is bad please don't play it",
+                "Have you ever considered touching grass instead?",
+                "🤮",
+                "I suggest you to do anything but play League",
+                "Hey, that's good and all but don't touch that game please",
+                "Think of the children!",
+                "Yeah, how about no"
+            ]
+            await message.channel.send(random.choice(league_response))
+
+        # Imagine.
+        elif re.search(r"\b(imagine)\b", message.content, re.IGNORECASE) and message.author.id != self.bot.user.id:
+            response = random.choice(["Imagine.", "> i m a g i n e"])
+            await message.channel.send(response)
 
 def setup(bot):
     bot.add_cog(Events(bot))
--- utils/repo.py
+++ utils/repo.py
@@ -6,10 +6,11 @@ with open("data/config.json", "r", encoding="utf-8") as config:
     config = json.load(config)
 
 
-DEFAULT_PREFIX = config.get("default_prefix")
+DEFAULT_PREFIX = config["default_prefix"]
 COLOURS = [0xFFE4E1, 0x00FF7F, 0xD8BFD8, 0xDC143C, 0xFF4500, 0xDEB887, 0xADFF2F,
            0x800000, 0x4682B4, 0x006400, 0x808080, 0xA0522D, 0xF08080, 0xC71585,
            0xFFB6C1, 0x00CED1]  # Colours of embed borders
+REACTIONS = config["reaction_data"]
 
 
 # Get prefixes
