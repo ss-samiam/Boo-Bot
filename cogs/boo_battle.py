94d399d836e08c59fa469b8691d6f49eb31aeec6
ss-samaim <sam.suvichagorn@gmail.com>
2022-07-05 21:08:38 +1000

    Implemented basic groundwork for fighting another user. Battle command works

--- cogs/boo_battle.py
+++ cogs/boo_battle.py
@@ -7,11 +7,11 @@ import random
 from utils import repo
 from utils import stats_generator
 
+fieldnames = ["Guild_ID", "User_ID", "Class", "Strength", "Defense", "Speed"]
+
 
 def check_user_exist(guild_id, user_id):
     with open("data/stats.csv", "r") as file:
-        user_exist = False
-        fieldnames = ["Guild_ID", "User_ID", "Class", "Strength", "Defense", "Speed"]
         reader = csv.DictReader(file, fieldnames=fieldnames)
         for row in reader:
             if str(guild_id) == row["Guild_ID"] and str(user_id) == row["User_ID"]:
@@ -19,9 +19,112 @@ def check_user_exist(guild_id, user_id):
         return False
 
 
+class Player:
+    def __init__(self, name, _class, _str, _def, _spd):
+        self.name = name
+        self.health = 10
+        self._str = _str
+        self._def = _def
+        self._spd = _spd
+
+    def get_name(self):
+        return self.name
+
+    def get_health(self):
+        return self.health
+
+    def get_str(self):
+        return self._str
+
+    def get_def(self):
+        return self._def
+
+    def get_spd(self):
+        return self._str
+
+    def take_dmg(self, damage):
+        self.health = self.health - damage
+
+    def attack(self, opponent):
+        damage = self.get_str() * (100/(100 + opponent.get_def()))
+        opponent.take_dmg(damage)
+        return damage
+
+
 class Battle(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command()
+    async def battle(self, ctx, target=None):
+        guild_id = ctx.message.guild.id
+        caller = ctx.author
+        if not check_user_exist(guild_id, caller.id):
+            await ctx.send("You are not registered! Please register using the ``register`` command")
+            return
+
+        if target is None:
+            await ctx.send("Please tag a user to initiate a fight with them.")
+            return
+        elif len(ctx.message.mentions) > 2:
+            await ctx.send("You can only fight with another person!")
+            return
+
+        target = self.bot.get_user(int(target[2:-1]))
+        if caller == target:
+            await ctx.send("You can't fight yourself") # Add some random choices
+            return
+
+        if not check_user_exist(guild_id, target.id):
+            await ctx.send(f"{target.display_name} is not registered! Please register using the ``register`` command")
+
+        # Load stats of caller and target
+        with open("data/stats.csv", mode="r", newline="") as file:
+            reader = csv.DictReader(file, fieldnames=fieldnames)
+            for row in reader:
+                if str(guild_id) == row["Guild_ID"] and str(caller.id) == row["User_ID"]:
+                    caller_stats = row
+                if str(guild_id) == row["Guild_ID"] and str(target.id) == row["User_ID"]:
+                    target_stats = row
+
+        caller_name = caller.display_name
+        caller_class = caller_stats["Class"]
+        caller_str = int(caller_stats["Strength"])
+        caller_def = int(caller_stats["Defense"])
+        caller_spd = int(caller_stats["Speed"])
+        caller_player = Player(caller_name, caller_class, caller_str, caller_def, caller_spd)
+
+        target_name = target.display_name
+        target_class = target_stats["Class"]
+        target_str = int(target_stats["Strength"])
+        target_def = int(target_stats["Defense"])
+        target_spd = int(target_stats["Speed"])
+        target_player = Player(target_name, target_class, target_str, target_def, target_spd)
+
+        while target_player.health > 0 and caller_player.health > 0:
+            # Caller attacks first
+            if caller_spd > target_spd:
+                fst_attacker = caller_player
+                snd_attacker = target_player
+            # Target attacks first
+            else:
+                fst_attacker = target_player
+                snd_attacker = caller_player
+
+            fst_damage = fst_attacker.attack(snd_attacker)
+            await ctx.send(f"**{fst_attacker.name}** attacks **{snd_attacker.name}**")
+            await ctx.send(f"**{fst_attacker.name}** dealt **{round(fst_damage, 1)}** damage!")
+            await ctx.send(f"**{fst_attacker.name}**'s Health ♡: **{round(fst_attacker.health, 1)}** | **{snd_attacker.name}**'s Health ♡: **{round(snd_attacker.health, 1)}**")
+
+            snd_damage = snd_attacker.attack(fst_attacker)
+            await ctx.send(f"**{snd_attacker.name}** attacks **{fst_attacker.name}**")
+            await ctx.send(f"**{snd_attacker.name}** dealt **{round(snd_damage, 1)}** damage!")
+            await ctx.send(f"**{fst_attacker.name}**'s Health ♡: **{round(fst_attacker.health, 1)}** | **{snd_attacker.name}**'s Health ♡: **{round(snd_attacker.health, 1)}**")
+        if target_player.health < 0:
+            await ctx.send(f"**{target_player.name}** wins!")
+        else:
+            await ctx.send(f"**{caller_player.name}** wins!")
+
 
     @commands.command()
     async def stats(self, ctx, target=None):
@@ -33,7 +136,6 @@ class Battle(commands.Cog):
 
         display_stats = {}
         with open("data/stats.csv", mode="r", newline="") as file:
-            fieldnames = ["Guild_ID", "User_ID", "Class", "Strength", "Defense","Speed"]
             reader = csv.DictReader(file, fieldnames=fieldnames)
             for row in reader:
                 if row["Guild_ID"] == str(guild_id) and row["User_ID"] == str(target.id):
@@ -51,7 +153,7 @@ class Battle(commands.Cog):
             stats.add_field(name="SPD", value=display_stats["Speed"], inline=True)
             await ctx.send(embed=stats)
         else:
-            await ctx.send(f"User not registered, please register using the ``register`` command")
+            await ctx.send("User not registered, please register using the ``register`` command")
 
     @commands.command()
     async def register(self, ctx):
@@ -102,7 +204,6 @@ class Battle(commands.Cog):
         msg = await self.bot.wait_for('message', check=lambda message: message.author == ctx.author)
         if msg.content in ["y", "Y"]:
             new_rows = []
-            fieldnames = ["Guild_ID", "User_ID", "Class", "Strength", "Defense", "Speed"]
             with open("data/stats.csv", "r") as file:
                 reader = csv.DictReader(file, fieldnames=fieldnames)
                 # Update stats if the user already exists
