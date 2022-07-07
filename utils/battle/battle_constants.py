ecccf63209637947890013bf968786a2e123c38c
ss-samaim <sam.suvichagorn@gmail.com>
2022-07-07 23:43:36 +1000

    Added constants and organised files by grouping them

---
+++ utils/battle/battle_constants.py
@@ -0,0 +1,14 @@
+FIELDNAMES = ["Guild_ID", "User_ID", "XP", "Class", "Skills", "HP", "Strength", "Defense", "Speed"]
+
+XP_X = 0.1
+XP_Y = 2
+XP_GAIN = 150
+
+SKILLS = {
+    "Fireball": {
+        "atk": 1.1
+    },
+    "Stab": {
+        "atk": 1
+    }
+}
